44f233f7a76efae05ce95d7ff1a27b96bd5c9f7d
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2022-09-11 20:43:53 +0000

    Update main.py

--- betmaster/main.py
+++ betmaster/main.py
@@ -3,5 +3,11 @@
 import requests
 
 from extract_functions import extract_footlive_tomorrow, extract_team_scores
+from calculate_functions import over_under_evaluation, most_recent_over_under
 
-extract_team_scores('bkma-yerevan')
+scores = extract_team_scores('bkma-yerevan')
+ev = over_under_evaluation(scores.ga)
+most_recent_over_under(scores.ga)
+
+print(ev)
+
