b67336a34bd90021fc4ec3a48b6e5ca6161a095e
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2022-09-12 10:04:49 +0000

    Update main.py

--- betmaster/main.py
+++ betmaster/main.py
@@ -2,11 +2,13 @@
 # Amoh-Gyebi Ampofo
 import requests
 
-from extract_functions import extract_footlive_tomorrow, extract_team_scores
+from extract_functions import extract_footlive_tomorrow, extract_team_scores, extract_h2h
 from summary_functions import over_under_summary
 
-scores = extract_team_scores('bkma-yerevan')
-bkma_sum = over_under_summary(scores)
+# scores = extract_team_scores('ararat-yerevan')
+# bkma_sum = over_under_summary(scores)
 
-print(bkma_sum)
+extract_h2h('bkma-yerevan', 'ararat-yerevan')
+
+# print(bkma_sum)
 
