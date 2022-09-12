1e994c36f5d03622896c3521d29e6d9975e401ae
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2022-09-12 10:05:03 +0000

    Update summary_functions.py

--- betmaster/summary_functions.py
+++ betmaster/summary_functions.py
@@ -2,7 +2,9 @@
 # Amoh-Gyebi Ampofo
 from typing import Dict
 
+from extract_functions import extract_h2h
 from calculate_functions import over_under_evaluation, most_recent_over_under, predict_over_under
+
 
 def over_under_summary(scores) -> Dict:
     gfou = over_under_evaluation(scores.gf)
@@ -17,3 +19,7 @@ def over_under_summary(scores) -> Dict:
         'Recent GA(5)': mrga_5, 'Recent GA(10)': mrga_10,
         'Predicted GF(5)': p5, 'Predicted GF(10)': p10,
         'Predicted GA(5)': pa5, 'Predicted GA(10)': pa10}
+
+
+def h2h_summary(team1: str, team2: str) -> Dict:
+    goals, team_goals = extract_h2h(team1, team2)
