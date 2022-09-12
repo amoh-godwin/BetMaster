7b1bae09a9f53b802c5939c9b47dfe93f682e227
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2022-09-12 21:08:51 +0000

    Update main.py

--- betmaster/main.py
+++ betmaster/main.py
@@ -5,12 +5,12 @@ import requests
 from extract_functions import extract_footlive_tomorrow, extract_team_scores, extract_h2h
 from summary_functions import over_under_summary, h2h_summary
 
-scores = extract_team_scores('ararat-yerevan')
+scores = extract_team_scores('pyunik')
 scores_2 = extract_team_scores('bkma-yerevan')
 bkma_sum = over_under_summary(scores_2)
 ararat_sum = over_under_summary(scores)
 
-h2h_summary('ararat-yerevan', 'bkma-yerevan')
+print(h2h_summary('pyunik', 'bkma-yerevan'))
 
 print(bkma_sum)
 print(ararat_sum)
