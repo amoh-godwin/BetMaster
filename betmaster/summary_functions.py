73b2ae7ab2e5d77939eb5ec1494d77de5945dcdc
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2022-09-12 07:14:45 +0000

    Update summary_functions.py

--- betmaster/summary_functions.py
+++ betmaster/summary_functions.py
@@ -2,14 +2,18 @@
 # Amoh-Gyebi Ampofo
 from typing import Dict
 
-from calculate_functions import over_under_evaluation, most_recent_over_under
+from calculate_functions import over_under_evaluation, most_recent_over_under, predict_over_under
 
 def over_under_summary(scores) -> Dict:
     gfou = over_under_evaluation(scores.gf)
     gaou = over_under_evaluation(scores.ga)
     mrgf_5, mrgf_10 = most_recent_over_under(scores.gf)
     mrga_5, mrga_10 = most_recent_over_under(scores.ga)
+    p5, p10 = predict_over_under(scores.gf)
+    pa5, pa10 = predict_over_under(scores.ga)
     return {
         'GF': gfou, 'GA': gaou, 
         'Recent GF(5)': mrgf_5, 'Recent GF(10)': mrgf_10, 
-        'Recent GA(5)': mrga_5, 'Recent GA(10)': mrga_10}
+        'Recent GA(5)': mrga_5, 'Recent GA(10)': mrga_10,
+        'Predicted GF(5)': p5, 'Predicted GF(10)': p10,
+        'Predicted GA(5)': pa5, 'Predicted GA(10)': pa10}
