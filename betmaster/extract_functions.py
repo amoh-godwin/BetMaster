ae0503942e1af3bc481186611e2a368448497ea4
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2022-09-12 10:05:11 +0000

    Update extract_functions.py

--- betmaster/extract_functions.py
+++ betmaster/extract_functions.py
@@ -3,7 +3,9 @@
 import requests
 import re
 from dataclasses import asdict, dataclass
-from typing import List, Tuple
+from typing import List, Tuple, Dict
+
+from storage_functions import temp_store_games, get_store_team_games
 
 
 @dataclass
@@ -81,6 +83,30 @@ def extract_game(div: str, team_name: str) -> Game:
     return Game(id, date, team_name, t1, t2, s1, s2)
 
 
+def extract_h2h(team1: str, team2: str) -> Tuple[List, Dict]:
+    t1_games = get_store_team_games(team1)
+    t2_games = get_store_team_games(team2)
+    t1_ids = set()
+    t2_ids = set()
+    games = []
+    team_goals = {team1: set(), team2: set() }
+
+    for x in t1_games:
+        t1_ids.add(x['id'])
+    for y in t2_games:
+        t2_ids.add(y['id'])
+
+    inter = t1_ids.intersection(t2_ids)
+
+    for x in t1_games:
+        if x['id'] in inter:
+            team_goals[x['team1']].add(x['score1'])
+            team_goals[x['team2']].add(x['score2'])
+            games.append((x['score1'], x['score2']))
+
+    return (games, team_goals)
+
+
 def extract_team_data(div: str) -> List:
     id, date, s1, s2, t1, t2 = extract_data(div)
     teams = [Team(int(id), t1, int(date)), Team(int(id), t2, int(date))]
@@ -92,14 +118,14 @@ def extract_team_scores(name: str) -> Scores:
     games = []
     sgf = []
     sga = []
-    """ req = requests.get(f'http://www.footlive.com/team/{name}/')
+    req = requests.get(f'http://www.footlive.com/team/{name}/')
     conts = req.text
 
     with open('goals.txt', 'wb') as fb:
-        fb.write(req.content) """
+        fb.write(req.content)
 
-    with open('goals.txt', 'r') as fr:
-        conts = fr.read()
+    """ with open('goals.txt', 'r') as fr:
+        conts = fr.read() """
 
     reg = r"data-id=['|\"].*?['|\"] data-status=['|\"]FT['|\"]"
     reg += r".*?data-slug2=['|\"].*?['|\"]"
@@ -110,6 +136,7 @@ def extract_team_scores(name: str) -> Scores:
         sga.append(ga)
         games.append(asdict(extract_game(x, name)))
 
+    temp_store_games(name, games)
 
     return Scores(name, tuple(sgf), tuple(sga))
 
