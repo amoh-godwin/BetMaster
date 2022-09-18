1548b6ceeebd5c93d6b7b9b78b185272880b9601
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2022-09-18 20:25:44 +0000

    Create experiments.py

---
+++ betmaster/experiments.py
@@ -0,0 +1,14 @@
+
+# BTTS
+# qualification: (homeover: over 1.0, awayover: over 1.0)
+
+# Add over 1.5
+# where both homeover and awayover are not over 1.0 and hit over 1.5
+# if addition supports over 2.5 play
+
+
+# Success
+# over 1.5 for  predictions over > 1.9
+# unless forced or we had to play over 1.5 for team prediction over > 1.5
+# we call this high life algorithm
+pass
